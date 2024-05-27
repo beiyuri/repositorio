4f6589178a0b78b5a6cab3894e9a8e5670b96744
Ericdelucas <126726641+Ericdelucas@users.noreply.github.com>
2024-05-27 09:11:03 -0300

    Update main.py
    Alterado por Eric Van De Laxs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 nome = string(input('Digite seu nome: "))
 num1 = int(input("Digite o numero: "))
 num2 = int(input("Digite o segundo numero: "))
+num3 = int(input("Digite o terceiro numero: "))
 
-soma = num1+num2
-dif= num1-num2
-mult = num1*num2
-divi = num1/num2
+soma = num1+num2+num3
+dif= num1-num2-num3
+mult = num1*num2*num3
+divi = num1/num2/num3
 
 print(f"nome: {nome}")
 print(f"Soma: {soma}")
