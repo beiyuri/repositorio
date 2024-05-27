f5b7250dc1b2cc37f08b9e739f06fcd20086da27
Manuela Sayuri <107208462+beiyuri@users.noreply.github.com>
2024-05-27 09:01:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+nome = string(input('Digite seu nome: "))
 num1 = int(input("Digite o numero: "))
 num2 = int(input("Digite o segundo numero: "))
 
@@ -6,6 +7,7 @@ dif= num1-num2
 mult = num1*num2
 divi = num1/num2
 
+print(f"nome: {nome}")
 print(f"Soma: {soma}")
 print(f"Diferença: {dif}")
 print(f"Multiplicação: {mult}")
